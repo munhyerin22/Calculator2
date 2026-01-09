9a289d882ac4f681513cb2fe165a9451541a3492
이름 <>
2026-01-10 02:06:23 +0900

    step2 진행중, List에 저장이 안됨.

--- src/calculator_pack/App.java
+++ src/calculator_pack/App.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);
         System.out.println("=== 계산기 프로그램 ===");
 
         String exit = ""; // 변수를 루프 외부에서 선언해야한다.
-        double total;
+        double total=0;
+
         do {
             System.out.print("첫번째 숫자를 입혁하세요: ");
             int num1 = sc.nextInt();
@@ -19,27 +21,34 @@ public class App {
             System.out.print("두번째 숫자를 입혁하세요: ");
             int num2 = sc.nextInt();
 
-            switch (a) {
-                case '+': total = num1 + num2; break;
-                case '-': total = num1 - num2; break;
-                case '*': total = num1 * num2; break;
-                case '/':
-                    if (num2 != 0) {
-                        total = (double) num1 / num2;
-                    } else {
-                        System.out.println("0으로 나눌 수 없습니다.");
+            if(num1>=0||num2>=0) {
+                switch (a) {
+                    case '+':
+                        total = num1 + num2;
+                        break;
+                    case '-':
+                        total = num1 - num2;
+                        break;
+                    case '*':
+                        total = num1 * num2;
+                        break;
+                    case '/':
+                        if (num2 != 0) {
+                            total = (double) num1 / num2;
+                        } else {
+                            System.out.println("0으로 나눌 수 없습니다.");
+                            continue;
+                        }
+                        break;
+                    default:
+                        System.out.println("지원하지 않는 연산자입니다.");
                         continue;
-                    }
-                    break;
-                default:
-                    System.out.println("지원하지 않는 연산자입니다.");
-                    continue;
+                }
             }
-
             System.out.println(num1 + " " + a + " " + num2 + " = " + total);
             sc.nextLine();
 
-            System.out.print("계속계산하시겠습니까?(그만 계산할 시 exit를 입력해주세요.): ");
+            System.out.print("계속 계산하시겠습니까?(그만 계산할 시 exit를 입력해주세요.): ");
             exit = sc.nextLine();
 
         } while (!exit.equals("exit"));
