ac4762d7649c5faf058832e76a26b5bb50a978f1
이름 <>
2026-01-09 13:17:18 +0900

    계산기 프로그램 step1

---
+++ src/calculator_pack/Calculator.java
@@ -0,0 +1,10 @@
+package calculator_pack;
+
+public class Calculator {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        System.out.println("  ");
+
+    }
+
+}
